5771cd5968aa47f7fbf8719d659a5c9ab42daf20
3brahim Sam3h <ebrahimsameh16@gmail.com>
2022-07-04 11:41:30 +0200

    static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -142,3 +142,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
