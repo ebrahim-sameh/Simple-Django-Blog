fe2e25c4264b77f6bc3f3aa26a459da4e4c0510d
3brahim Sam3h <ebrahimsameh16@gmail.com>
2022-07-04 12:54:58 +0200

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rw!jm(x%ewigh&=eko7ik48lz)3dk375#^^+1=vcw310764b^('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangobloggy.herokuapp.com']
 
 # Application definition
 
